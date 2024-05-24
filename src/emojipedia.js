58cc9ac15b31e010b91660cc230f8ffc3cd1f522
Utkarsh <utkarshgayguwal123@gmail.com>
2024-05-24 10:42:04 +0530

    All Files of Emojipedia

---
+++ src/emojipedia.js
@@ -0,0 +1,60 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id: 5,
+    emoji: "😄",
+    name: "Smiling Face With Open Mouth",
+    meaning:
+      "It is often used to indicate humorous situations, funny jokes, or moments of joy. In chats, the emoji can also be sent along with sarcasm or ironic remarks to emphasize the humorous side of a message."
+  },
+  {
+    id: 6,
+    emoji: "😁",
+    name: "Smiling Face With Teeth Showing",
+    meaning:
+      "It is often used to show that you are happy about an event, excited, or just a little embarrassed. Joking and cheerfulness are other meanings associated with this emoji."
+  },
+  {
+    id: 7,
+    emoji: "🥹",
+    name: "Face Holding Back Tears",
+    meaning:
+      " It is often used to express strong feelings in communication, and the emotional interpretation depends on the context.  In conversation, this emoji can be used to convey empathy and compassion, especially when discussing emotional topics."
+  },
+  {
+    id: 8,
+    emoji: "😇",
+    name: " Smiling Face With Halo",
+    meaning:
+      "People use it to express their good deeds, loving behavior or exemplary attitude. Sometimes this emoji is used humorously to show a winking innocence for less good deeds or behaviors."
+  }
+];
+
+export default emojipedia;
